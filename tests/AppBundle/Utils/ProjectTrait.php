33414c4d31b1e9bfbcd6221902dd349d1ee0c6a4
mbuliard <phalange14>
2018-05-16 21:54:32 +0200

    projectServiceTest

---
+++ tests/AppBundle/Utils/ProjectTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/**
+ *
+ *  * This is an iumio component [https://iumio.com]
+ *  *
+ *  * (c) Mickael Buliard <mickael.buliard@iumio.com>
+ *  *
+ *  * Bill&Go, gérer votre administratif efficacement [https://billandgo.fr]
+ *  *
+ *  * To get more information about licence, please check the licence file
+ *
+ */
+
+namespace Tests\AppBundle\Utils;
+
+use BillAndGoBundle\Entity\Project;
+use BillAndGoBundle\Entity\User;
+
+/**
+ * Trait ProjectTrait
+ * @package Tests\AppBundle\Utils
+ */
+trait ProjectTrait
+{
+
+    /**
+     * @param User $user
+     * @param array $data
+     * @return Project
+     */
+    private function createProject (User $user, array $data = []) : Project
+    {
+        $project = new Project();
+        $project->setRefUser($user);
+        $project->setName('macron');
+        $project->setDescription("C'est notre projeeeeeeeeet");
+        $project->setBegin(new \DateTime());
+        $project->setDeadline(new \DateTime());
+        return $project;
+    }
+}
